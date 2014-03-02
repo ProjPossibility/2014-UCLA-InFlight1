4f6204910c02111d2e911728103d43dadb69ef73
Howard C Lee <howardchlee@gmail.com>
2014-03-02 09:02:46 -0800

    add header to node

--- node.js
+++ node.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
 	req.on("end", function()
 	{	
 		var _get = url.parse(req.url, true).query;
-		res.writeHead(200, {'Content-Type':'text/plain'});
+		res.writeHead(200, {'Content-Type':'text/plain', "Access-Control-Allow-Origin":"*"});
 		if(_get['data'] == "movies")
 		{
 			var ret = "{\"movies\":[\n";
@@ -52,5 +52,5 @@ http.createServer(function (req, res) {
 		}
 	});
 	req.resume();
-}).listen(1337, '127.0.0.1');
+}).listen(1337);
 console.log('Server running at http://127.0.0.1:1337/');
